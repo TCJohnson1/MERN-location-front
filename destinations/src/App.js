c1a6adf891095e9f12d8036efe68b32d608de8ed
sarakendig <sarakendig@gmail.com>
2020-11-17 16:17:22 -0500

    structure of header component built

--- destinations/src/App.js
+++ destinations/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Header from './components/Header'
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1> MERN Destinations</h1>
       </div>
     )
---
+++ destinations/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <nav class="teal darken-4">
+          <div class="nav-wrapper">
+          
+            <a href="#!" class="brand-logo teal-text lighten-5">
+            <i class=" large material-icons deep-orange-text accent-4">airplanemode_active</i>DREAM DESTINATIONS
+            </a>
+            <a href="#1" data-target="mobile-demo" class="sidenav-trigger">
+              <i class="material-icons deep-orange-text accent-4">menu</i>
+            </a>
+            <ul class="right hide-on-med-and-down">
+              <li>
+                <a href="#1" class="teal-text lighten-5">Home</a>
+              </li>
+              <li>
+                <a href="#1" class="teal-text lighten-5">My Favorites</a>
+              </li>
+              <li>
+                <a href="#1" class="teal-text lighten-5">Signup/Login</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <ul class="sidenav" id="mobile-demo">
+          <li>
+            <a href="#1" class="teal-text lighten-5">Home</a>
+          </li>
+          <li>
+            <a href="#1" class="teal-text lighten-5">My Favorites</a>
+          </li>
+          <li>
+            <a href="#1" class="teal-text lighten-5">Signup/Login</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
