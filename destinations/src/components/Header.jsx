8a4f9b8b947f6c6e35695d5b6b2c38b3ace53cb3
david ornelas <davidornelas_11@hotmail.com>
2020-11-18 10:48:28 -0700

    added browser router

--- destinations/src/components/Header.jsx
+++ destinations/src/components/Header.jsx
@@ -4,38 +4,38 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <nav class="teal darken-4">
-          <div class="nav-wrapper">
+        <nav className="teal darken-4">
+          <div className="nav-wrapper">
           
-            <a href="#!" class="brand-logo teal-text lighten-5">
-            <i class=" large material-icons deep-orange-text accent-4">airplanemode_active</i>DREAM DESTINATIONS
+            <a href="#!" className="brand-logo teal-text lighten-5">
+            <i className=" large material-icons deep-orange-text accent-4">airplanemode_active</i>DREAM DESTINATIONS
             </a>
-            <a href="#1" data-target="mobile-demo" class="sidenav-trigger">
-              <i class="material-icons deep-orange-text accent-4">menu</i>
+            <a href="#1" data-target="mobile-demo" className="sidenav-trigger">
+              <i className="material-icons deep-orange-text accent-4">menu</i>
             </a>
-            <ul class="right hide-on-med-and-down">
+            <ul className="right hide-on-med-and-down">
               <li>
-                <a href="#1" class="waves-effect waves-light teal-text lighten-5">Home</a>
+                <a href="#1" className="waves-effect waves-light teal-text lighten-5">Home</a>
               </li>
               <li>
-                <a href="#1" class="waves-effect waves-light teal-text lighten-5">My Favorites</a>
+                <a href="#1" className="waves-effect waves-light teal-text lighten-5">My Favorites</a>
               </li>
               <li>
-                <a href="#1" class="waves-effect waves-light teal-text lighten-5">Signup/Login</a>
+                <a href="#1" className="waves-effect waves-light teal-text lighten-5">Signup/Login</a>
               </li>
             </ul>
           </div>
         </nav>
 
-        <ul class="sidenav" id="mobile-demo">
+        <ul className="sidenav" id="mobile-demo">
           <li>
-            <a href="#1" class="waves-effect waves-light teal-text lighten-5">Home</a>
+            <a href="#1" className="waves-effect waves-light teal-text lighten-5">Home</a>
           </li>
           <li>
-            <a href="#1" class="waves-effect waves-light teal-text lighten-5">My Favorites</a>
+            <a href="#1" className="waves-effect waves-light teal-text lighten-5">My Favorites</a>
           </li>
           <li>
-            <a href="#1" class="waves-effect waves-light teal-text lighten-5">Signup/Login</a>
+            <a href="#1" className="waves-effect waves-light teal-text lighten-5">Signup/Login</a>
           </li>
         </ul>
       </header>
