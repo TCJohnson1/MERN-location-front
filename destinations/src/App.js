e6c8eb74406cd09ed3a78dc701378eaa771314e8
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-18 15:37:14 -0500

    created and styled my list button

--- destinations/src/App.js
+++ destinations/src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react'
-import Footer from './components/Footer'
-import Header from './components/Header'
-
+import React, { Component } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 export default class App extends Component {
   render() {
     return (
-     
       <div>
-     
-      <Header />
-      
+        <Header />
+
         <main>
-        <h1> MERN Destinations</h1>
+          <div className="my-list-button">
+            <button id="my-list-button">My List</button>
+          </div>
         </main>
 
         <Footer />
-    
-        
       </div>
-      
-    )
+    );
   }
 }
