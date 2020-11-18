3980f4e5e9e994293e06d0f5d5b8bcb138490a68
tcjohnosn2 <terrence.c.johnson2@gmail.com>
2020-11-18 16:27:07 -0600

    Starting MVP

--- destinations/src/App.js
+++ destinations/src/App.js
@@ -4,11 +4,29 @@ import Header from './components/Header'
 
 
 export default class App extends Component {
+
+  constructor (props) { //Google Map API
+    super(props)
+    this.state = {
+      // baseURL: 'http://www.xxxxxxx.com/?',
+      // apikey: 'apikey=' + 'xxxxxxx',
+      // query: '&t=',
+      // locationTitle: '',
+      // searchURL: '',
+      // location: {}
+      name: '',
+      description: '',
+      image: '',
+      favotires: []
+    }
+  }
+  //The first API will pull the information and have the geo location. We can then pull that geotag and plug it into the Google maps API to show the pin. Google-maps-react
+
   render() {
     return (
-     
+
       <div>
-     
+
       <Header />
       
         <main>
---
+++ destinations/src/components/Favorites.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Favorites extends Component {
+      render() {
+            return (
+                  <div>
+                        <h3>Favorites</h3>
+                        <table>
+                              <thead>
+                                    <tr>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    </tr>
+                              </thead>
+                        </table>
+                  </div>
+            )
+      }
+}
