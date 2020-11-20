fe97133d7c2db47b520c7d65c4b8159a070d79ee
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-19 19:30:09 -0500

    finished setting up react router

--- destinations/src/App.js
+++ destinations/src/App.js
@@ -1,65 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Landing from "./components/Landing";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
         <Header />
 
         <main>
-          <div className="my-list-button">
-            <button
-              id="my-list-button"
-              class="btn waves-effect waves-orange darken-4"
-              type="submit"
-              name="action"
-            >
-              My List
-            </button>
-          </div>
-          <div className="cool-image">
-            <img
-              id="cool-image"
-              src="https://cdn.pixabay.com/photo/2016/06/25/12/48/go-pro-1478810_1280.jpg"
-              alt="luggage"
-            ></img>
-          </div>
-          <hr className="horizontal-bar"></hr>
-          <div className="random-destination-button">
-            <button
-              id="random-destination-button"
-              class="btn waves-effect waves-orange darken-4"
-              type="submit"
-              name="action"
-            >
-              Destination Roulette
-            </button>
-          </div>
-          <div className="find-destination">
-            <form>
-              <h5>Find by Name</h5>
-              <input
-                className="name-of-destination"
-                id="name-of-destination"
-                type="text"
-                placeholder="Name of Destination"
-              ></input>
-              <button
-                id="add-to-list-button"
-                class="btn waves-effect waves-orange darken-4"
-                type="submit"
-                name="action"
-              >
-                Add to my List
-              </button>
-            </form>
-          </div>
+          <Switch>
+            <Route path="/" exact component={Landing} />
+          </Switch>
         </main>
 
         <Footer />
-      </div>
+      </Router>
     );
   }
 }
