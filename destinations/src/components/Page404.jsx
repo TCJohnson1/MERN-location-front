9c41a159629bf92c40b1a9062a08f117ad65d93b
sarakendig <sarakendig@gmail.com>
2020-11-20 11:53:14 -0500

    added a cutom logo and some styling to the 404 page

--- destinations/src/components/Page404.jsx
+++ destinations/src/components/Page404.jsx
@@ -11,9 +11,13 @@ export default class Page404 extends Component {
                <main>
                <div  className='container'>
                <center>
-            <h1>
-                404 PAGE NOT FOUND <br/>
-                <Link to='/'> <div className='waves-effect waves-light btn orange darken-4 white-text'> Home Page</div></Link>
+            <h1 className='deep-orange-text'>
+                
+                404 PAGE NOT FOUND<br/>
+                 <img src='https://i.imgur.com/hwj3i2x.png?1'/>
+                    <br/>
+                
+                <Link to='/'> <div className='waves-effect waves-light btn-large orange darken-4 white-text'> Home Page</div></Link>
             </h1>
             </center>
             </div>
